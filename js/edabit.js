a63f2db93d49461cb9fc5a1e52198a1e467d5f83
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2020-10-23 22:33:19 -0500

    more edabit practice

--- js/edabit.js
+++ js/edabit.js
@@ -66,3 +66,14 @@ function isSameNum(num1, num2) {
         return days
     }
 }
+// This challenge is a bit different as the function you are given already contains some code that you should not change or remove. Also, don't use a return statement, it is already included. Your task is, given an object, prevent new properties from being added to that object, but you should still be able to change the value of the existing properties.
+function signYourName(obj) {
+    // write your code here
+    // don't use a return statement
+obj.seal(obj)
+
+    // DON'T CHANGE OR REMOVE THE LINES BELOW
+    obj.yourSignature = "Whatever";
+    obj.spouseSignature = "Nice Try"
+    return obj;
+}
